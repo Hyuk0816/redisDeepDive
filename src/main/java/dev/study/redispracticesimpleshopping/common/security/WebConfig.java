352f961ec72b8631bae9bd780a7d9c95f4de02eb
Hyuk <rlawogur816@gmail.com>
2025-01-21 23:05:58 +0900

    feature: 현재 유저 정보를 들고오는 애논테이션 추가 AOP 적용

---
+++ src/main/java/dev/study/redispracticesimpleshopping/common/security/WebConfig.java
@@ -0,0 +1,21 @@
+package dev.study.redispracticesimpleshopping.common.security;
+
+import dev.study.redispracticesimpleshopping.oauth.util.UserInfoMethodArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+	private final UserInfoMethodArgumentResolver userInfoMethodArgumentResolver;
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(userInfoMethodArgumentResolver);
+	}
+}
